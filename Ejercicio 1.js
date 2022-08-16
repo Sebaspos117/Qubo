ee148cc2506d60837344394a9d92e5538ec774d8
Sebaspos117 <sporrass@student.invenio.ac.cr>
2022-08-15 20:14:16 -0600

    Ejercicio 2

---
+++ Ejercicio 1.js
@@ -0,0 +1,12 @@
+const list = ["A", 1, 4, 5, "C", "B", 6, 8, "G", "D", 7, 3, "F", 2];
+const list1 = [];
+const list2 = [];
+for (let i = 1; i <list.length; i++){
+    if(typeof list[i] =='string') {
+        list1.push(list[i])}
+    else{
+    list2.push(list[i])}
+}
+list1.sort()
+list2.sort()
+console.log(list1, list2);
